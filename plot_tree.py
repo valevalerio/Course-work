da2a9124c84d4f76b6952c063e3cd5af6727e250
valevalerio <valerio12bonsignori@gmail.com>
2020-09-14 16:33:44 +0200

    Update plot_tree.py

--- plot_tree.py
+++ plot_tree.py
@@ -74,15 +74,16 @@ def PlotTree(obliqueT,filename="OT1Text",visual=True,features=None,classes = Non
     g = Digraph('G', filename = filename,format = 'png')
     DFSOblique(radix=obliqueT._root,UseRule=not visual,classes_names=classes,graph=g)
     g.view()
-    '''import os
-    os.system("rm [0-9]*.png")
-    os.remove("-1.png")
-    '''
+    
     
     import cv2
     img = cv2.imread(filename+'.png')
     plt.figure(figsize = (20, 20))
+    plt.axis('off')
     plt.imshow(img)
+    import os
+    os.system("rm [0-9]*.png")
+    os.remove("-1.png")
     return g
     
 
